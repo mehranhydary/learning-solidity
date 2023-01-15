a03706fc7a2e772d9abe3690d3b8a7d0364a2eda
mehranhydary <mehranhydary@users.noreply.github.com>
2023-01-14 20:51:40 -0500

    Adding some functions

--- src/Uniswap/v2/Swap.sol
+++ src/Uniswap/v2/Swap.sol
@@ -1,4 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Swap {}
+import {IERC20, Uniswap} from "v2-core/interfaces/";
+
+contract Swap {
+  address private constant UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+  address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+  function swap(
+    address _tokenIn,
+    address _tokenOut,
+    uint _amountIn, 
+    uint _amountOutMin,
+    address _to
+  ) external {
+    IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
+    IERC20(_tokenIn).approve(UNISWAP_V2_ROUTER, _amountIn);
+  }
+}
